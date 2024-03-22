ba7703bd695d3c6e31b5362335d2c59adabab441
sammyniyo <samshakul@gmail.com>
2024-03-22 11:37:55 +0200

    making routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
+
+//import router dom
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><Login/></div>
+  },
+  {
+    path: '/Register',
+    element: <div><register/></div>
+  },
+  {
+    path: '/Dashboard',
+    element: <div><dashboard/></div>
+  },
+])
 
 function App() {
  
-
   return (
     <div>
- App
+    <RouterProvider router={router}/>
     </div>
   )
 }
